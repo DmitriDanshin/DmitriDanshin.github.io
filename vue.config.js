4f34e78f17ffe2bb8bb67e5aa71a6db474f6aa2f
Grisehyrdene <Dmitri.Danshin2017@gmail.com>
2021-06-01 20:55:51 +0300

    Menu, Resume, Works and Skills added

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,8 @@ module.exports = {
                 prependData: '@import "@assets/styles.scss";'
             }
         }
-    }
+    },
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/DmitriDanshin/'
+        : '/'
 }
